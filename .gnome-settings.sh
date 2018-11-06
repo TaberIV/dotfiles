e05dc0d839064fe7c7028956a3d2571a04832916
Taber McFarlin <tabermcfarlin@gmail.com>
2018-11-05 19:10:57 -0500

    Enable dual screen workspaces
    That's right, I made a commit from the GitHub editor, fight me.

--- .gnome-settings.sh
+++ .gnome-settings.sh
@@ -5,3 +5,6 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "[]"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "[]"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "[]"
+
+# Enable dual screen workspaces
+gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
